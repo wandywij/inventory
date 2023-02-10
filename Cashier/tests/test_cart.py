3adddf3b55bc4cab85ff076655263310796365a5
wandysunterra <wandy.wijayanto@sunterra.id>
2023-02-11 00:42:54 +0700

    add reset_transaction() unit test

--- Cashier/tests/test_cart.py
+++ Cashier/tests/test_cart.py
@@ -1,7 +1,7 @@
 from src import item
 from src import cart
 
-def test_cart_items():
+def test__add_cart_items():
     cart_items = cart.Cart()
     
     new_item_to_be_added = item.Item(item_name= "Teh Kotak", qty= 2, price= 3500)
@@ -15,3 +15,14 @@ def test_cart_items():
     assert(len(cart_items.items) == 2)
     assert(cart_items.items[0].item_name == "Teh Kotak")
     assert(cart_items.items[1].item_name == "Cheetos")
+
+
+def test_clear_cart_items():
+    cart_items = cart.Cart()
+    
+    new_item_to_be_added = item.Item(item_name= "Teh Kotak", qty= 2, price= 3500)
+    cart_items.addItem(new_item_to_be_added)
+    new_item_to_be_added = item.Item(item_name= "Cheetos", qty= 3, price= 1700)
+    cart_items.addItem(new_item_to_be_added)
+    cart_items.reset_transaction()
+    assert(len(cart_items.items) == 0)
